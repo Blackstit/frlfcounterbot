89c0d92766e5d1de6300ec0af240c60682eff841
Vladislav Petrushkin <56411253+Blackstit@users.noreply.github.com>
2024-03-11 02:58:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -89,17 +89,6 @@ def message_handler(update, context):
 def me(update, context):
     # Получаем идентификатор пользователя, отправившего сообщение
     user_id = update.message.from_user.id
-    
-    # Подключение к базе данных MySQL
-    mydb = mysql.connector.connect(
-        host=MYSQL_HOST,
-        user=MYSQL_USER,
-        password=MYSQL_PASSWORD,
-        database=MYSQL_DATABASE,
-        port=MYSQL_PORT
-    )
-
-    cursor = mydb.cursor()
 
     # Выполняем запрос к базе данных
     cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
@@ -137,14 +126,15 @@ def me(update, context):
 
         # Получаем количество сообщений пользователя из таблицы user_stats
         cursor.execute("SELECT message_count FROM user_stats WHERE user_id = %s", (user_id,))
-        message_count = cursor.fetchone()[0] if cursor.rowcount else 0
+        message_count_result = cursor.fetchone()
+        message_count = message_count_result[0] if message_count_result else 0
 
         # Получаем дату последней активности пользователя из таблицы user_stats
         cursor.execute("SELECT last_message_date FROM user_stats WHERE user_id = %s ORDER BY last_message_date DESC LIMIT 1", (user_id,))
-        last_activity_date_result = cursor.fetchall()
-        
+        last_activity_date_result = cursor.fetchone()
+
         if last_activity_date_result:
-            last_activity_date = last_activity_date_result[0][0]  # Получаем первую дату из результатов
+            last_activity_date = last_activity_date_result[0]  # Получаем первую дату из результатов
             last_activity_date_str = last_activity_date.strftime("%Y-%m-%d %H:%M:%S")  # Преобразуем в строку
             last_activity_formatted = last_activity_date_str.strftime("%d.%m.%Y")  # Форматируем дату
         else:
@@ -157,79 +147,6 @@ def me(update, context):
         context.bot.send_message(chat_id=update.message.chat_id, text=profile_message, reply_to_message_id=update.message.message_id)
     else:
         context.bot.send_message(chat_id=update.message.chat_id, text="Вы еще не зарегистрированы")
-
-    cursor.close()
-    mydb.close()
-
-
-# Функция обработки команды /top
-def top(update, context):
-    # Получаем топ-10 пользователей по количеству репутации
-    cursor.execute("SELECT username, reputation FROM users ORDER BY reputation DESC LIMIT 10")
-    top_users = cursor.fetchall()
-
-    if top_users:
-        # Формируем сообщение с топ-10 пользователями
-        top_message = "Топ 10 холдеров $AGAVA:\n\n"
-        for index, user in enumerate(top_users, start=1):
-            username = user[0]
-            reputation = user[1]
-            top_message += f"{index}. @{username} - {reputation} $AGAVA\n"
-
-        # Отправляем сообщение с топ-10 пользователями
-        context.bot.send_message(chat_id=update.message.chat_id, text=top_message)
-    else:
-        context.bot.send_message(chat_id=update.message.chat_id, text="Пока нет пользователей с репутацией")
-
-
-# Функция обработки команды /give
-def give(update, context):
-    # Получаем ID пользователя, отправившего команду
-    sender_user_id = update.message.from_user.id
-
-    # Получаем количество токенов, которое отправляется
-    if len(context.args) == 1:
-        try:
-            tokens_to_give = int(context.args[0])
-        except ValueError:
-            context.bot.send_message(chat_id=update.message.chat_id, text="Некорректное количество токенов.")
-            return
-    else:
-        context.bot.send_message(chat_id=update.message.chat_id, text="Укажите количество токенов для передачи.")
-        return
-
-    # Получаем баланс отправителя
-    cursor.execute("SELECT reputation FROM users WHERE id = %s", (sender_user_id,))
-    sender_balance = cursor.fetchone()[0]
-
-    # Проверяем, достаточно ли у пользователя баланса для передачи токенов
-    if sender_balance >= tokens_to_give:
-        # Выбираем случайного пользователя для начисления токенов
-        cursor.execute("SELECT id, reputation FROM users WHERE id != %s", (sender_user_id,))
-        all_users = cursor.fetchall()
-        random_user_id, _ = random.choice(all_users)
-
-        # Начисляем токены случайному пользователю
-        cursor.execute("UPDATE users SET reputation = reputation + %s WHERE id = %s", (tokens_to_give, random_user_id))
-
-        # Списываем токены у отправителя
-        cursor.execute("UPDATE users SET reputation = reputation - %s WHERE id = %s", (tokens_to_give, sender_user_id))
-        mydb.commit()
-
-        # Получаем username случайного пользователя
-        cursor.execute("SELECT username FROM users WHERE id = %s", (random_user_id,))
-        random_username = cursor.fetchone()[0]
-
-        # Получаем username пользователя, отправившего команду
-        sender_username = update.message.from_user.username
-
-        # Формируем сообщение
-        message_text = f"@{random_username}, вам случайным образом @{sender_username} начислил +{tokens_to_give} $AGAVA"
-
-        # Отправляем сообщение
-        context.bot.send_message(chat_id=update.message.chat_id, text=message_text)
-    else:
-        context.bot.send_message(chat_id=update.message.chat_id, text="Недостаточно токенов на балансе.")
 
 
 # Токен вашего бота
@@ -247,14 +164,6 @@ dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), message
 # Регистрируем обработчик команды /me
 me_handler = CommandHandler('me', me)
 dispatcher.add_handler(me_handler)
-
-# Регистрируем обработчик команды /top
-top_handler = CommandHandler('top', top)
-dispatcher.add_handler(top_handler)
-
-# Регистрируем обработчик команды /give
-give_handler = CommandHandler('give', give)
-dispatcher.add_handler(give_handler)
 
 # Запускаем бота
 updater.start_polling()
