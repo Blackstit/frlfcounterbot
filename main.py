aedc518d5666f9b9ccd06e2a15ac68b39882cf77
Vladislav Petrushkin <56411253+Blackstit@users.noreply.github.com>
2024-03-11 16:00:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
+import pymongo
 import os
-from dotenv import load_dotenv
-from pymongo import MongoClient
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from datetime import datetime
+from dotenv import load_dotenv
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
 
-# Получение переменных окружения
+# Получение переменных окружения для подключения к MongoDB
 MONGO_HOST = os.getenv("MONGOHOST")
-MONGO_USER = os.getenv("MONGO_INITDB_ROOT_USERNAME")
-MONGO_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
-MONGO_PORT = os.getenv("MONGOPORT")
+MONGO_USER = os.getenv("MONGOUSER")
+MONGO_PASSWORD = os.getenv("MONGOPASSWORD")
+MONGO_PORT = int(os.getenv("MONGOPORT"))
+MONGO_URL = os.getenv("MONGO_URL")
 
 # Подключение к MongoDB
-client = MongoClient(MONGO_HOST, username=MONGO_USER, password=MONGO_PASSWORD, port=int(MONGO_PORT))
-db = client["telegram_bot_db"]  # Имя вашей базы данных
-
-# Получение коллекций
-user_stats_collection = db["user_stats"]
+client = pymongo.MongoClient(MONGO_URL)
+db = client['test']  # Замените 'your_database_name' на имя вашей базы данных
+user_stats_collection = db['user_stats']  # Коллекция для статистики пользователей
 
 # Функция для обработки сообщений пользователя
 def message_handler(update, context):
@@ -30,11 +29,14 @@ def message_handler(update, context):
 
     try:
         # Проверка, существует ли уже запись о пользователе в базе данных
-        user_data = user_stats_collection.find_one({"user_id": user_id})
+        user_data = user_stats_collection.find_one({'user_id': user_id})
 
         if user_data:
             # Если запись о пользователе существует, обновляем количество сообщений и дату последнего сообщения
-            user_stats_collection.update_one({"user_id": user_id}, {"$inc": {"message_count": 1}, "$set": {"last_message_date": message_date}})
+            user_stats_collection.update_one(
+                {'user_id': user_id},
+                {'$inc': {'message_count': 1}, '$set': {'last_message_date': message_date}}
+            )
         else:
             # Если запись о пользователе отсутствует, удаляем сообщение пользователя
             context.bot.delete_message(chat_id=chat_id, message_id=update.message.message_id)
@@ -47,12 +49,17 @@ def message_handler(update, context):
             return
 
         # Получаем текущее количество сообщений пользователя
-        message_count = user_stats_collection.find_one({"user_id": user_id})["message_count"]
+        user_data = user_stats_collection.find_one({'user_id': user_id})
+        if user_data:
+            message_count = user_data.get('message_count', 0)
 
-        # Если количество сообщений кратно 10, начисляем 1 очко репутации
-        if message_count % 10 == 0:
-            # Увеличиваем репутацию на 1
-            user_stats_collection.update_one({"user_id": user_id}, {"$inc": {"reputation": 1}})
+            # Если количество сообщений кратно 10, начисляем 1 очко репутации
+            if message_count % 10 == 0:
+                # Увеличиваем репутацию на 1 и обновляем запись в базе данных
+                user_stats_collection.update_one(
+                    {'user_id': user_id},
+                    {'$inc': {'reputation': 1}}
+                )
 
     except Exception as e:
         print("Error handling message:", e)
@@ -63,24 +70,18 @@ def me(update, context):
         # Получаем идентификатор пользователя, отправившего сообщение
         user_id = update.message.from_user.id
 
-        # Ищем данные пользователя в коллекции
-        user_data = user_stats_collection.find_one({"user_id": user_id})
+        # Получаем данные пользователя из базы данных
+        user_data = user_stats_collection.find_one({'user_id': user_id})
 
         if user_data:
-            referrals_count = user_data.get("referrals_count", "Нет")
-            username = user_data.get("username", "Нет")
-            message_count = user_data.get("message_count", 0)
-            last_activity_date = user_data.get("last_message_date", "Нет данных")
-            reputation = user_data.get("reputation", 0)
-
-            # Получаем текущую дату и время
-            current_datetime = datetime.now()
-
-            # Вычисляем разницу в днях между текущей датой и датой регистрации
-            days_since_registration = (current_datetime - user_data["registration_date"]).days
+            referrals_count = user_data.get('referrals_count', 0)
+            reputation = user_data.get('reputation', 0)
+            username = user_data.get('username', 'Нет')
+            message_count = user_data.get('message_count', 0)
+            last_activity_date = user_data.get('last_message_date', 'Нет данных')
 
             # Формируем сообщение профиля с учетом количества сообщений, репутации и информации о пригласившем пользователе
-            profile_message = f"Имя пользователя: @{username}\nДней в боте: {days_since_registration}\nПоследняя активность: {last_activity_date}\nРеферралы: {referrals_count}\nКоличество сообщений: {message_count}\nБаланс: {reputation}\n\n"
+            profile_message = f"Имя пользователя: @{username}\nКоличество сообщений: {message_count}\nРепутация: {reputation}\nПоследняя активность: {last_activity_date}"
 
             # Создаем инлайн клавиатуру с кнопкой "Открыть бот"
             keyboard = [[InlineKeyboardButton("Открыть бот 🤖", url="t.me/Cyndycate_invaterbot?start=yjkqU3t1U8")]]
@@ -89,7 +90,7 @@ def me(update, context):
             # Отправляем сообщение с профилем пользователя, используя реплай на сообщение, которое вызвало команду /me
             context.bot.send_message(chat_id=update.message.chat_id, text=profile_message, reply_to_message_id=update.message.message_id, reply_markup=reply_markup)
         else:
-            context.bot.send_message(chat_id=update.message.chat_id, text="Вы еще не зарегистрированы")
+            context.bot.send_message(chat_id=update.message.chat_id, text="Вы еще не зарегистрированы", reply_to_message_id=update.message.message_id)
 
     except Exception as e:
         print("Error handling /me command:", e)
