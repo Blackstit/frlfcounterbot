e12535034b485975e34b499e94af203b7dac20b2
Blackstit <vladstit@gmail.com>
2024-03-12 18:18:36 +0800

    code-refaktoring

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ def connect_to_database():
     client = pymongo.MongoClient(MONGO_URL)
     db = client['test']
 
-    users_stats_collection = db.get_collection('users_stats', create=True)  # Коллекция для статистики пользователей
+    users_stats_collection = db.get_collection('users_stats')  # Коллекция для статистики пользователей
     users_collection = db.get_collection('users')  # Коллекция для пользователей
     commands_collection = db.get_collection('commands')  # Коллекция для статистики пользователей
     tasks_collection = db.get_collection('completed_tasks')  # Коллекция для выполненных заданий
